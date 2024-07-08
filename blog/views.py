bfd4bb407c941312f27ece08e38d6c6b984bbce7
Philip Lian <philip.lian1@yahoo.com>
2024-07-08 17:21:17 -0400

    change html for website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# create post_list function, take request as argument/parameter
+
+def post_list(request):
+    # return the request, HTML files you want to send.
+    return render(request, 'blog/post_list.html', {} )
